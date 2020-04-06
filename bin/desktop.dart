9941ebd5d0bfc43ed4d33fcb2904709fc03cbdfd
Kibria92 <kibria92@gmail.com>
2020-04-06 14:57:05 +0600

    start learn Dart programming

---
+++ bin/desktop.dart
@@ -0,0 +1,15 @@
+void main2() {
+  print("snd method");
+}
+ void main() {
+
+  var fName ="GOLAM";
+   var lName ="kibria";
+   print(fName.toLowerCase()+" "+fName.toUpperCase());
+  if(fName==lName){
+    print("this is equal");
+  }else if(fName!=lName){
+    print("This is no equal");
+  }
+
+}
